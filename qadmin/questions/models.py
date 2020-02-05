3488500ed62cb7a1886b26734281e60210f0223a
Xing He <xingh.dll@gmail.com>
2020-02-05 23:21:27 +0800

    questions in db ready

--- qadmin/questions/models.py
+++ qadmin/questions/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    CHOICES = (
+        (11, 'Algorithm'),
+        (12, 'Data Structure'),
+        (13, 'Trick'),
+        (14, 'Programming'),
+        (15, 'Games'),
+        (21, 'Operatiing System'),
+        (32, 'Control Theory'),
+    )
+
+    title = models.CharField(max_length=200)
+    categories = models.IntegerField(choices=CHOICES, default=11)
+    description = models.TextField()
+    solution = models.TextField()
