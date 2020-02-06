f4bbea22096d839b89a5fd7177a2c5db13f943cc
Xing He <xingh.dll@gmail.com>
2020-02-06 10:21:29 +0800

    Altered question.categories model type

--- qadmin/questions/models.py
+++ qadmin/questions/models.py
@@ -2,17 +2,8 @@ from django.db import models
 
 
 class Question(models.Model):
-    CHOICES = (
-        (11, 'Algorithm'),
-        (12, 'Data Structure'),
-        (13, 'Trick'),
-        (14, 'Programming'),
-        (15, 'Games'),
-        (21, 'Operatiing System'),
-        (32, 'Control Theory'),
-    )
-
     title = models.CharField(max_length=200)
-    categories = models.IntegerField(choices=CHOICES, default=11)
+    # Comma separated category list.
+    categories = models.CharField(max_length=100)
     description = models.TextField()
     solution = models.TextField()
